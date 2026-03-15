fd6083358ff5013cc09097738894845868fd1cf8
Cristian Meira <cristian.meira15@gmail.com>
2026-03-15 11:51:36 -0300

    Added user interface

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
+from unicodedata import category
+
 from Models.Asset import Asset
 from Models.Portfolio import Portfolio
 from Service.Rebalancer import Rebalancer
 from Strategies.AllocationStrategy import AllocationStrategy
+from Util.util import category_input, valid_input_assets
 
-print("PORTFOLIO REBALANCE \n")
+print("PORTFOLIO REBALANCE REPORT\n")
 
-client1 = Portfolio("Cristian")
-asset1 = Asset("RTN18", "stocks", 1500, 20)
-asset2 = Asset("NAYS1", "reits", 698, 30)
-asset3 = Asset("JKS1A", "bonds", 1239, 70)
-client1.add_asset(asset1)
-client1.add_asset(asset2)
-client1.add_asset(asset3)
+cliente_name = input("Client name: ")
+client1 = Portfolio(cliente_name)
 
-allocationStrategy = AllocationStrategy(60,25,15)
+num = valid_input_assets()
+for i in range(num):
+    print("\nAsset [{}]: ".format(i+1))
+    ticket = input("Ticket: ")
+    category = category_input()
+    price = float(input("Price: "))
+    quantity = int(input("Quantity: "))
+    asset = Asset(ticket,category,price,quantity)
+    client1.add_asset(asset)
+
+print("\nAllocation strategy: ")
+stocks = int(input("Stocks: "))
+reits = int(input("REITs: "))
+bonds = int(input("Bonds: "))
+
+allocationStrategy = AllocationStrategy(stocks, reits, bonds)
 clientRebalance = Rebalancer(client1, allocationStrategy)
 
+print("\n===========================")
 print("Portfolio from {}:\n".format(client1.get_owner()))
 print("Total portfolio value: ${:.2f}\n".format(client1.total_value()))
 
@@ -33,3 +47,4 @@ print("Bonds: {:.2f}%".format(targetAllocation[2]),"\n")
 
 print("System sugestion: ")
 clientRebalance.rebalance_sugestions()
+print("===========================")
